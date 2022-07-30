96fc6ef739ea9a146047cbaf0dbbe0440fa20491
el1ght <el1ghtifyoucan@gmail.com>
2022-07-30 17:19:23 +0300

    eslint & prettier enabled

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Qualitie from "./qualitie";
 import BookMark from "./bookmark";
+import PropTypes from "prop-types";
 
 const maxRate = 5;
 
-const User = ({ _id, name, qualities, profession, completedMeetings, rate, onDelete, bookmark, onToggleBookMark }) => {
+const User = ({
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    onDelete,
+    bookmark,
+    onToggleBookMark
+}) => {
     return (
         <tr>
             <td>{name}</td>
@@ -15,15 +26,37 @@ const User = ({ _id, name, qualities, profession, completedMeetings, rate, onDel
             </td>
             <td>{profession.name}</td>
             <td>{completedMeetings}</td>
-            <td>{rate} /{maxRate}</td>
             <td>
-                <BookMark status={bookmark} onClick={() => onToggleBookMark(_id)} />
+                {rate} /{maxRate}
             </td>
             <td>
-                <button className='btn btn-danger' onClick={() => onDelete(_id)}>delete</button>
+                <BookMark
+                    status={bookmark}
+                    onClick={() => onToggleBookMark(_id)}
+                />
+            </td>
+            <td>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => onDelete(_id)}
+                >
+                    delete
+                </button>
             </td>
         </tr>
-    )
-}
+    );
+};
+
+User.propTypes = {
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array.isRequired,
+    profession: PropTypes.object.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    bookmark: PropTypes.bool.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
+};
 
 export default User;
